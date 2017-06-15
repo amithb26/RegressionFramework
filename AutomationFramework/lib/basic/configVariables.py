36d151c5c575cf387feaa037c2e2c16262320a31
cqtbo <amit6c@gmail.com>
2017-06-15 14:50:30 +0530

    Add files via upload
    Python And Robot Scripts

---
+++ AutomationFramework/lib/basic/configVariables.py
@@ -0,0 +1,6 @@
+#This file holds the variables required for handling convergence
+#delays and fault control.
+
+LinkmaxVal = 10     # [In seconds]Maximum time the test retries to bring the port UP.
+LinkStepVal = 2     # [In seconds]Step value for which the test sleeps before retrying.
+LinkStartVal = 2    # [In seconds]Start value the test should sleep before first check.
